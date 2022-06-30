34713e66b6200072fb29da4224b8f6fd44760576
alejandrosatxc <alejandro@satxconsultants.com>
2022-06-30 16:17:10 -0500

    Substituded CSS grid over bootstrap
    Swapped out bootstrap for a dynamic chart layout using CSS grid display property, there seems to be a bug in safari browsers (safari can't reliably use grid-column-flow), still trying to figure out how to make grid items span multiple rows if the space is available.

--- pages/polls/[poll].js
+++ pages/polls/[poll].js
@@ -5,6 +5,7 @@ import PieChart from '../../components/PieChart'
 import BarChart from '../../components/BarChart'
 import { Container, Row, Col } from 'react-bootstrap';
 import { useRouter } from 'next/router'
+import styles from '../../styles/Home.module.css'
 
 export async function getStaticPaths() {
     return {
@@ -39,7 +40,7 @@ export default function Poll({ master }) {
     let chartData = require('/charts.json')
     for (const chart in chartData) {
         combineDataPoints(
-            chartData[chart], 
+            chartData[chart],
             generateChartDatasets(chartData[chart].columns, master[pollNum].data)
         )
     }
@@ -171,182 +172,122 @@ export default function Poll({ master }) {
     }
 
     return (
-        <Container fluid>
-            <Row>
-                <Col lg={6} xs={12} >
-                    <PieChart
-                        title="How do you plan on voting in this year's elections?"
-                        column={chartData.chartVotingPlans.columns}
-                        masterDataset={master[pollNum].data}
-                        dataset={chartData.chartVotingPlans.config.data}
-                        options={pieOptions}
-                    />
-                </Col>
-                <Col lg={6} xs={12}>
-                    <PieChart
-                        title="Would you vote yes or no on a 150 million dollar City of San Antonio bond for Affordable Housing Projects that includes rehabilitating, preserving and producing housing for homeownership or rent, and supportive services for people exiting homelessness?"
-                        column={chartData.chartAHP.columns}
-                        masterDataset={master[pollNum].data}
-                        dataset={chartData.chartAHP.config.data}
-                        options={pieOptions}
-                    />
-                </Col>
-            </Row>
-            <Row>
-                <Col xs={12}>
-                    <BarChart
-                        title="Some say these issues are problems in the greater San Antonio area. Do you think it's a serious problem, somewhat serious problem or not a serious problem?"
-                        columns={chartData.chartProblems.columns}
-                        masterDataset={master[pollNum].data}
-                        dataset={chartData.chartProblems.config.data}
-                        options={barOptions_stacked}
-                    />
-                </Col>
-            </Row>
-            <Row>
-                <Col xs={12}>
-                    <h2>For the following 3 charts, respondents were asked:  Which government level do you think should take the lead in addressing the stated issue?</h2>
-                </Col>
-            </Row>
-            <Row>
-                <Col xs={12}>
-                    <BarChart
-                        title="Local Priorities"
-                        columns={chartData.chartLP.columns}
-                        masterDataset={master[pollNum].data}
-                        dataset={chartData.chartLP.config.data}
-                        options={barOptions_stacked}
-                    />
-                </Col>
-            </Row>
-            <Row>
-                <Col xs={12}>
-                    <BarChart
-                        title="Federal Priorities"
-                        columns={chartData.chartFP.columns}
-                        masterDataset={master[pollNum].data}
-                        dataset={chartData.chartFP.config.data}
-                        options={barOptions_stacked}
-                    />
-                </Col>
-            </Row>
-            <Row>
-                <Col xs={12}>
-                    <BarChart
-                        title="Split on which level of government should lead"
-                        columns={chartData.chartS.columns}
-                        masterDataset={master[pollNum].data}
-                        dataset={chartData.chartS.config.data}
-                        options={barOptions_stacked}
-                    />
-                </Col>
-            </Row>
-            <Row>
-                <Col lg={6} xs={12}>
-                    <PieChart
-                        title="The Cantril Self-Anchoring Scale is a tool used to assess the well-being of a population by asking respondents to rate where their life stands now, and where they think it will be in five years, using a hypothetical ten-point “ladder” scale. By combining a respondents rating we are able able to identify if people feel they are Thriving, Struggling, or Suffering."
-                        column={chartData.chartLife.columns}
-                        masterDataset={master[pollNum].data}
-                        dataset={chartData.chartLife.config.data}
-                        options={pieOptions}
-                    />
-                </Col>
-                <Col lg={6} xs={12}>
-                    <PieChart
-                        title="How would you describe your current financial situation?"
-                        column={chartData.chartFinance.columns}
-                        masterDataset={master[pollNum].data}
-                        dataset={chartData.chartFinance.config.data}
-                        options={pieOptions}
-                    />
-                </Col>
-            </Row>
-            <Row>
-                <Col xs={12}>
-                    <BarChart
-                        title="Have you experienced any of the following over the last 12 months?"
-                        columns={chartData.chartExperiences.columns}
-                        masterDataset={master[pollNum].data}
-                        dataset={chartData.chartExperiences.config.data}
-                        options={barOptions_stacked}
-                    />
-                </Col>
-            </Row>
-            <Row>
-                <Col xs={12}>
-                    <BarChart
-                        title="Do you think these things contribute to the rising cost of living in Bexar County?"
-                        columns={chartData.chartCOL.columns}
-                        masterDataset={master[pollNum].data}
-                        dataset={chartData.chartCOL.config.data}
-                        options={barOptions_stacked}
-                    />
-                </Col>
-            </Row>
-            <Row className="justify-content-md-center">
-                <Col lg={6} xs={12} className="w-100">
-                    <PieChart
-                        title='Do you agree or disagree with the following statement "Having reliable, high quality child care for young children is crucial for parents to be able to work?"'
-                        column={chartData.chartStatement.columns}
-                        masterDataset={master[pollNum].data}
-                        dataset={chartData.chartStatement.config.data}
-                        options={pieOptions}
-                    />
-                </Col>
-            </Row>
-            <Row>
-                <Col xs={12}>
-                    <BarChart
-                        title="What do you think is a challenge when looking for a job?"
-                        columns={chartData.chartChallenges.columns}
-                        masterDataset={master[pollNum].data}
-                        dataset={chartData.chartChallenges.config.data}
-                        options={barOptions_stacked}
-                    />
-                </Col>
-            </Row>
-            <Row>
-                <Col xs={12}>
-                    <BarChart
-                        title="How important do you think each of the following factors are to a person's health?"
-                        columns={chartData.chartHealth.columns}
-                        masterDataset={master[pollNum].data}
-                        dataset={chartData.chartHealth.config.data}
-                        options={barOptions_stacked}
-                    />
-                </Col>
-            </Row>
-            <Row>
-                <Col lg={6} xs={12}>
-                    <PieChart
-                        title="Have you provided any monetary support to a charitable or non-profit organization in the last year?"
-                        column={chartData.chartCharity.columns}
-                        masterDataset={master[pollNum].data}
-                        dataset={chartData.chartCharity.config.data}
-                        options={pieOptions}
-                    />
-                </Col>
-                <Col lg={6} xs={12}>
-                    <PieChart
-                        title="Have you or anyone in your household provided volunteer support to a charitable or non-profit organization in the last year?"
-                        column={chartData.chartVolunteer.columns}
-                        masterDataset={master[pollNum].data}
-                        dataset={chartData.chartVolunteer.config.data}
-                        options={pieOptions}
-                    />
-                </Col>
-            </Row>
-            <Row className="justify-content-md-center">
-                <Col lg={6} xs={12} className="w-100">
-                    <PieChart
-                        title="Were some or all of your charitable donations in 2021 benefiting a community in Bexar County?"
-                        column={chartData.chartDonations.columns}
-                        masterDataset={master[pollNum].data}
-                        dataset={chartData.chartDonations.config.data}
-                        options={pieOptions}
-                    />
-                </Col>
-            </Row>
-        </Container>
+        <div className={styles.chartGrid}>
+            <PieChart
+                title="How do you plan on voting in this year's elections?"
+                column={chartData.chartVotingPlans.columns}
+                masterDataset={master[pollNum].data}
+                dataset={chartData.chartVotingPlans.config.data}
+                options={pieOptions}
+            />
+            <PieChart
+                title="Would you vote yes or no on a 150 million dollar City of San Antonio bond for Affordable Housing Projects that includes rehabilitating, preserving and producing housing for homeownership or rent, and supportive services for people exiting homelessness?"
+                column={chartData.chartAHP.columns}
+                masterDataset={master[pollNum].data}
+                dataset={chartData.chartAHP.config.data}
+                options={pieOptions}
+            />
+            <BarChart
+                title="Some say these issues are problems in the greater San Antonio area. Do you think it's a serious problem, somewhat serious problem or not a serious problem?"
+                columns={chartData.chartProblems.columns}
+                masterDataset={master[pollNum].data}
+                dataset={chartData.chartProblems.config.data}
+                options={barOptions_stacked}
+            />
+            <div className={styles.chartGroup}>
+                <h2>For the following 3 charts, respondents were asked:  Which government level do you think should take the lead in addressing the stated issue?</h2>
+                <BarChart
+                    title="Local Priorities"
+                    columns={chartData.chartLP.columns}
+                    masterDataset={master[pollNum].data}
+                    dataset={chartData.chartLP.config.data}
+                    options={barOptions_stacked}
+                />
+                <BarChart
+                    title="Federal Priorities"
+                    columns={chartData.chartFP.columns}
+                    masterDataset={master[pollNum].data}
+                    dataset={chartData.chartFP.config.data}
+                    options={barOptions_stacked}
+                />
+                <BarChart
+                    title="Split on which level of government should lead"
+                    columns={chartData.chartS.columns}
+                    masterDataset={master[pollNum].data}
+                    dataset={chartData.chartS.config.data}
+                    options={barOptions_stacked}
+                />
+            </div>
+            <PieChart
+                title="The Cantril Self-Anchoring Scale is a tool used to assess the well-being of a population by asking respondents to rate where their life stands now, and where they think it will be in five years, using a hypothetical ten-point “ladder” scale. By combining a respondents rating we are able able to identify if people feel they are Thriving, Struggling, or Suffering."
+                column={chartData.chartLife.columns}
+                masterDataset={master[pollNum].data}
+                dataset={chartData.chartLife.config.data}
+                options={pieOptions}
+            />
+            <PieChart
+                title="How would you describe your current financial situation?"
+                column={chartData.chartFinance.columns}
+                masterDataset={master[pollNum].data}
+                dataset={chartData.chartFinance.config.data}
+                options={pieOptions}
+            />
+            <BarChart
+                title="Have you experienced any of the following over the last 12 months?"
+                columns={chartData.chartExperiences.columns}
+                masterDataset={master[pollNum].data}
+                dataset={chartData.chartExperiences.config.data}
+                options={barOptions_stacked}
+            />
+            <BarChart
+                title="Do you think these things contribute to the rising cost of living in Bexar County?"
+                columns={chartData.chartCOL.columns}
+                masterDataset={master[pollNum].data}
+                dataset={chartData.chartCOL.config.data}
+                options={barOptions_stacked}
+            />
+            <PieChart
+                title='Do you agree or disagree with the following statement "Having reliable, high quality child care for young children is crucial for parents to be able to work?"'
+                column={chartData.chartStatement.columns}
+                masterDataset={master[pollNum].data}
+                dataset={chartData.chartStatement.config.data}
+                options={pieOptions}
+            />
+            <BarChart
+                title="What do you think is a challenge when looking for a job?"
+                columns={chartData.chartChallenges.columns}
+                masterDataset={master[pollNum].data}
+                dataset={chartData.chartChallenges.config.data}
+                options={barOptions_stacked}
+            />
+            <BarChart
+                title="How important do you think each of the following factors are to a person's health?"
+                columns={chartData.chartHealth.columns}
+                masterDataset={master[pollNum].data}
+                dataset={chartData.chartHealth.config.data}
+                options={barOptions_stacked}
+            />
+            <PieChart
+                title="Have you provided any monetary support to a charitable or non-profit organization in the last year?"
+                column={chartData.chartCharity.columns}
+                masterDataset={master[pollNum].data}
+                dataset={chartData.chartCharity.config.data}
+                options={pieOptions}
+            />
+            <PieChart
+                title="Have you or anyone in your household provided volunteer support to a charitable or non-profit organization in the last year?"
+                column={chartData.chartVolunteer.columns}
+                masterDataset={master[pollNum].data}
+                dataset={chartData.chartVolunteer.config.data}
+                options={pieOptions}
+            />
+            <PieChart
+                title="Were some or all of your charitable donations in 2021 benefiting a community in Bexar County?"
+                column={chartData.chartDonations.columns}
+                masterDataset={master[pollNum].data}
+                dataset={chartData.chartDonations.config.data}
+                options={pieOptions}
+            />
+        </div>
     )
 }
