39d3b7df1b5d63a23b30ade7b8f1203c474f5eef
alejandrosatxc <alejandro@satxconsultants.com>
2022-07-01 16:44:20 -0500

    removed keys from charts.json
    Created a copy of charts.json --> keyless.json which is an array of chart objects, edited the loop in the poll component to operate on that array of chart objects.

--- pages/polls/[poll].js
+++ pages/polls/[poll].js
@@ -37,13 +37,13 @@ export default function Poll({ master }) {
     //Loop through all chart objects, generate chart datasets, then assign
     //each chart's dataset.data values to the generated dataset. 
     //This  should set up all charts to be rendered into react components
-    let chartData = require('/charts.json')
-    for (const chart in chartData) {
+    let chartData = require('/keyless.json')
+    chartData.forEach(chart => {
         combineDataPoints(
-            chartData[chart],
-            generateChartDatasets(chartData[chart].columns, master[pollNum].data)
+            chart,
+            generateChartDatasets(chart.columns, master[pollNum].data)
         )
-    }
+    })
 
     var barOptions_stacked = {
         //responsive: true,
@@ -173,121 +173,31 @@ export default function Poll({ master }) {
 
     return (
         <div className={styles.chartGrid}>
-            <PieChart
-                title="How do you plan on voting in this year's elections?"
-                column={chartData.chartVotingPlans.columns}
-                masterDataset={master[pollNum].data}
-                dataset={chartData.chartVotingPlans.config.data}
-                options={pieOptions}
-            />
-            <PieChart
-                title="Would you vote yes or no on a 150 million dollar City of San Antonio bond for Affordable Housing Projects that includes rehabilitating, preserving and producing housing for homeownership or rent, and supportive services for people exiting homelessness?"
-                column={chartData.chartAHP.columns}
-                masterDataset={master[pollNum].data}
-                dataset={chartData.chartAHP.config.data}
-                options={pieOptions}
-            />
-            <BarChart
-                title="Some say these issues are problems in the greater San Antonio area. Do you think it's a serious problem, somewhat serious problem or not a serious problem?"
-                columns={chartData.chartProblems.columns}
-                masterDataset={master[pollNum].data}
-                dataset={chartData.chartProblems.config.data}
-                options={barOptions_stacked}
-            />
-            <div className={styles.chartGroup}>
-                <h2>For the following 3 charts, respondents were asked:  Which government level do you think should take the lead in addressing the stated issue?</h2>
-                <BarChart
-                    title="Local Priorities"
-                    columns={chartData.chartLP.columns}
-                    masterDataset={master[pollNum].data}
-                    dataset={chartData.chartLP.config.data}
-                    options={barOptions_stacked}
-                />
-                <BarChart
-                    title="Federal Priorities"
-                    columns={chartData.chartFP.columns}
-                    masterDataset={master[pollNum].data}
-                    dataset={chartData.chartFP.config.data}
-                    options={barOptions_stacked}
-                />
-                <BarChart
-                    title="Split on which level of government should lead"
-                    columns={chartData.chartS.columns}
-                    masterDataset={master[pollNum].data}
-                    dataset={chartData.chartS.config.data}
-                    options={barOptions_stacked}
-                />
-            </div>
-            <PieChart
-                title="The Cantril Self-Anchoring Scale is a tool used to assess the well-being of a population by asking respondents to rate where their life stands now, and where they think it will be in five years, using a hypothetical ten-point “ladder” scale. By combining a respondents rating we are able able to identify if people feel they are Thriving, Struggling, or Suffering."
-                column={chartData.chartLife.columns}
-                masterDataset={master[pollNum].data}
-                dataset={chartData.chartLife.config.data}
-                options={pieOptions}
-            />
-            <PieChart
-                title="How would you describe your current financial situation?"
-                column={chartData.chartFinance.columns}
-                masterDataset={master[pollNum].data}
-                dataset={chartData.chartFinance.config.data}
-                options={pieOptions}
-            />
-            <BarChart
-                title="Have you experienced any of the following over the last 12 months?"
-                columns={chartData.chartExperiences.columns}
-                masterDataset={master[pollNum].data}
-                dataset={chartData.chartExperiences.config.data}
-                options={barOptions_stacked}
-            />
-            <BarChart
-                title="Do you think these things contribute to the rising cost of living in Bexar County?"
-                columns={chartData.chartCOL.columns}
-                masterDataset={master[pollNum].data}
-                dataset={chartData.chartCOL.config.data}
-                options={barOptions_stacked}
-            />
-            <PieChart
-                title='Do you agree or disagree with the following statement "Having reliable, high quality child care for young children is crucial for parents to be able to work?"'
-                column={chartData.chartStatement.columns}
-                masterDataset={master[pollNum].data}
-                dataset={chartData.chartStatement.config.data}
-                options={pieOptions}
-            />
-            <BarChart
-                title="What do you think is a challenge when looking for a job?"
-                columns={chartData.chartChallenges.columns}
-                masterDataset={master[pollNum].data}
-                dataset={chartData.chartChallenges.config.data}
-                options={barOptions_stacked}
-            />
-            <BarChart
-                title="How important do you think each of the following factors are to a person's health?"
-                columns={chartData.chartHealth.columns}
-                masterDataset={master[pollNum].data}
-                dataset={chartData.chartHealth.config.data}
-                options={barOptions_stacked}
-            />
-            <PieChart
-                title="Have you provided any monetary support to a charitable or non-profit organization in the last year?"
-                column={chartData.chartCharity.columns}
-                masterDataset={master[pollNum].data}
-                dataset={chartData.chartCharity.config.data}
-                options={pieOptions}
-            />
-            <PieChart
-                title="Have you or anyone in your household provided volunteer support to a charitable or non-profit organization in the last year?"
-                column={chartData.chartVolunteer.columns}
-                masterDataset={master[pollNum].data}
-                dataset={chartData.chartVolunteer.config.data}
-                options={pieOptions}
-            />
-            <PieChart
-                title="Were some or all of your charitable donations in 2021 benefiting a community in Bexar County?"
-                column={chartData.chartDonations.columns}
-                masterDataset={master[pollNum].data}
-                dataset={chartData.chartDonations.config.data}
-                options={pieOptions}
-            />
+            {
+                chartData.map(chart => {
+                    if (chart.type === 'pie') {
+                        return (
+                            <PieChart
+                                title={chart.config.data.title}
+                                column={chart.columns}
+                                masterDataset={master[pollNum.data]}
+                                dataset={chart.config.data}
+                                options={pieOptions}
+                            />
+                        )
+                    } else if (chart.type === 'bar') {
+                        return (
+                            <BarChart
+                                title={chart.config.data.title}
+                                columns={chart.columns}
+                                masterDataset={master[pollNum.data]}
+                                dataset={chart.config.data}
+                                options={barOptions_stacked}
+                            />
+                        )
+                    }
+                })
+            }
         </div>
     )
 }
