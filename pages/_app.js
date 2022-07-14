f40a1650a41646291cbf8a147ef60ef059cc3ed1
vanesarivera555 <vanesarivera555@gmail.com>
2022-07-14 18:56:39 -0500

    Redesigned navigation to used imported react-bootstrap components

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/globals.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from '../styles/Home.module.css'
 import Head from 'next/head'
 import Image from 'next/image'
@@ -8,85 +8,62 @@ import Navbar from 'react-bootstrap/Navbar'
 import { Container } from 'react-bootstrap';
 import SideNav from '../components/SideNav';
 import MobileNav from '../components/MobileNav';
-
+import NavBJ from '../components/NavBJ';
+import Script from 'next/script';
 function MyApp({ Component, pageProps }) {
-  const openNav = (e) => {
-  document.getElementById("mySidenav").style.width = "250px";
-  // document.getElementById("main-app-container").style.marginLeft = "250px"; 
-  }
-  const openMobileNav = (e) => {
-    document.getElementById("myMobilenav").style.height = "250px";
-    // document.getElementById("main-app-container").style.marginTop= "250px"; 
-    // document.getElementById("openMobile").style.display = "none";
-    }
 
-  return (<div>
-    <SideNav />
-    <MobileNav/>
-    <div id="main-app-container" className={styles.container}>
-      {/* <Navbar className={styles.nav} sticky='top'>
-          <Container>
-            <Navbar.Brand>
-            <a
+
+  return (
+    <div>
+    
+      <div id="main-app-container" className={styles.container}>
+        <Head>
+          <title>Bexar Dash</title>
+          <meta name="description" content="Bexar Facts' Data Dashboard!" />
+          <link rel="icon" href="/favicon.ico" />
+          <meta charset="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous" />
+
+        </Head>
+        <NavBJ />
+        <main className={styles.main}>
+
+          {/* <Button id ="openBtn" className = {styles.navBtn} onClick={(e)=>{openNav()}}>OpenNav</Button> */}
+          <h1 className={styles.title}>
+            Welcome to Bexar-Dash
+          </h1>
+
+          <div className={styles.description}>
+            A data dashboard by&nbsp;
+            <Image src="/bf-1.png" alt="Vercel Logo" width={164} height={32} />
+            ,&nbsp;visualizing Bexar Facts polling data
+          </div>
+
+
+          <div className={styles.menuButtons}>
+            <Button className={styles.poll7} variant='poll7' href="/polls/7">NEW - Poll 7 Results!</Button>
+            <Button className={styles.localgov} variant='localgov' href="/issues/localgovernment">Local Government</Button>
+            <Button className={styles.electedofc} variant='electedofc' href="/issues/electedofficials">Elected Officials</Button>
+          </div>
+          <p className={styles.disclaimer}>This dashboard is currently formatted for viewing on desktop or laptop only. We will make it mobile friendly overtime.</p>
+          <Component {...pageProps} />
+        </main>
+        <footer className={styles.footer}>
+          <a
             href="https://bexarfacts.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-              <Image
-                alt=""
-                src="/BF-Logo.jpeg"
-                width={330}
-                height={80}
-                className="d-inline-block align-top"
-              />
-              </a>
-            </Navbar.Brand>
-          </Container>
-      </Navbar> */}
-      <Head>
-        <title>Bexar Dash</title>
-        <meta name="description" content="Bexar Facts' Data Dashboard!" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-          <div className={styles.openB} onClick={(e)=>{openNav()}}>&#9776; </div>        
-          <div className={styles.openMobileB} id = "openMobile" onClick={(e)=>{openMobileNav()}}>&#9776; </div>        
-
-         {/* <Button id ="openBtn" className = {styles.navBtn} onClick={(e)=>{openNav()}}>OpenNav</Button> */}
-        <h1 className={styles.title}>
-          Welcome to Bexar-Dash
-        </h1>
-
-        <div className={styles.description}>
-          A data dashboard by&nbsp;
-          <Image src="/bf-1.png" alt="Vercel Logo" width={164} height={32} />
-          ,&nbsp;visualizing Bexar Facts polling data
-        </div>
-
-
-        <div className={styles.menuButtons}>
-          <Button className={styles.poll7} variant='poll7' href="/polls/7">NEW - Poll 7 Results!</Button>
-          <Button className={styles.localgov} variant='localgov' href="/issues/localgovernment">Local Government</Button>
-          <Button className={styles.electedofc} variant='electedofc' href="/issues/electedofficials">Elected Officials</Button>
-        </div>
-        <p className={styles.disclaimer}>This dashboard is currently formatted for viewing on desktop or laptop only. We will make it mobile friendly overtime.</p>
-        <Component {...pageProps} />
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://bexarfacts.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/bf-1.png" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+            Powered by{' '}
+            <span className={styles.logo}>
+              <Image src="/bf-1.png" alt="Vercel Logo" width={72} height={16} />
+            </span>
+          </a>
+        </footer>
+      </div>
     </div>
-  </div>
   )
 }
 
