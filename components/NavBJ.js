db628736f517159819706de8181be69e3dcef4e6
alejandrosatxc <alejandro@satxconsultants.com>
2022-08-02 21:03:08 -0500

    Set href targets

--- components/NavBJ.js
+++ components/NavBJ.js
@@ -1,4 +1,3 @@
-import $ from 'jquery';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Container from 'react-bootstrap/Container';
@@ -11,34 +10,29 @@ const NavBJ = (props) => {
 
     return (
         <Navbar className = {styles.bg} collapseOnSelect expand="lg"  variant="dark" sticky="top" >
-        <Navbar.Brand className = {styles.navbarbrand} href="#home">
+        <Navbar.Brand className = {styles.navbarbrand} href="https://bexarfacts.org">
         <Image src = "/bf-11.png" 
-                width="220"
+                width="280"
                 height = "40"
                 alt=""/> 
         </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
-              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/issues/localgovernment">Local Government</Nav.Link>
+              <Nav.Link href="/issues/electedofficials">Elected Officials</Nav.Link>
+              <NavDropdown title="Polls" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="/polls/7">Poll 7</NavDropdown.Item>
+                {/* <NavDropdown.Item href="#action/3.2">Local Government</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Elected Officials</NavDropdown.Item> */}
               </NavDropdown>
             </Nav>
             <Nav>
-              <Nav.Link href="#deets">More deets</Nav.Link>
+              {/* <Nav.Link href="#deets">More deets</Nav.Link>
               <Nav.Link eventKey={2} href="#memes">
                 Dank memes
-              </Nav.Link>
+              </Nav.Link> */}
             </Nav>
           </Navbar.Collapse>
       </Navbar>
--- pages/_app.js
+++ pages/_app.js
@@ -23,8 +23,8 @@ function MyApp({ Component, pageProps }) {
           <link rel="icon" href="/favicon.ico" />
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossOrigin="anonymous" />
 
         </Head>
         <NavBJ />
