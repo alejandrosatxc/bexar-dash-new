ec01bbb64f59eeaa058b65b43ad4029b66370ffe
alejandrosatxc <alejandro@satxconsultants.com>
2022-08-02 22:25:10 -0500

    Added some text for homepage

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,8 @@ export default function Home({ master }) {
 
   let chartData = require('/keyless.json')
   combineDataPoints(chartData[0], generateChartDatasets(chartData[0].columns, master[6].data))
+  combineDataPoints(chartData[1], generateChartDatasets(chartData[1].columns, master[6].data))
+
 
   const options = {
     "barOptions_stacked": {
@@ -148,47 +150,70 @@ export default function Home({ master }) {
 
   return (
     <div className={styles.wrapper}>
-      <h2 className={styles.header}>Tap on a button above to see the relevant data</h2>
-      <div className={styles.box1}>BexarDash-BOX1</div>
+      {/* <h2 className={styles.header}>Tap on a button above to see the relevant data</h2> */}
+      <div className={styles.header}>
+        When we equip our communities with knowledge, not only can we make better decisions,
+        our voices become more effective and powerful. The data collected by Bexar Facts provided
+        community insight into what our voting neighbors think:
+        <ul>
+          <li>Are the most serious problems facing our City </li>
+          <li>About the direction of the City and County</li>
+          <li>Regarding the approval of our local leaders, civic organizations and institutions</li>
+          <li>Around police relations and reform</li>
+          <li>About local government's response to the pandemic and the winter storm</li>
+          <li>About labor challenges</li>
+          <li>About the health disparities in our community</li>
+        </ul>
+      </div>
+      <div style={{textAlign: 'center'}}>
+        <h3>
+          How can Bexar Facts impact public policy? Bexar Facts data creates opportunities to advocate for public
+          policy by understanding and use of the data.
+        </h3>
+        <p>
+          Our organization utilizes public opinion polling combined with independent
+          demographic research to compile empirical data on issues, attitudes, and
+          trends shaping the Greater San Antonio Area. Providing free and accessible
+          data to our community and driving civic engagement and participation are cornerstones of our efforts.
+        </p>
+      </div>
       <div>
-        Box222-Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet et vitae omnis, iusto cupiditate commodi
-        debitis sunt praesentium quis. Repellendus tenetur expedita eius. Et quos provident architecto numquam vitae
-        cumque voluptatem quaerat, delectus recusandae officia nesciunt quod tempore harum dolores nobis neque
-        veniam est eveniet dolore assumenda eos culpa! Pariatur, iste nobis, corrupti doloribus optio numquam, eum
-        nihil reiciendis deleniti maxime explicabo a harum. Architecto unde vitae pariatur tempora ab veniam
-        asperiores, facere quod beatae, sint vero aut dolorem quis.
-        Box2-Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet et vitae omnis, iusto cupiditate commodi
-        debitis sunt praesentium quis. Repellendus tenetur expedita eius. Et quos provident architecto numquam vitae
-        cumque voluptatem quaerat, delectus recusandae officia nesciunt quod tempore harum dolores nobis neque
-        veniam est eveniet dolore assumenda eos culpa! Pariatur, iste nobis, corrupti doloribus optio numquam, eum
-        nihil reiciendis deleniti maxime explicabo a harum. Architecto unde vitae pariatur tempora ab veniam
-        asperiores, facere quod beatae, sint vero aut dolorem quis.</div>
-      <div>  <PieChart
-        title={chartData[0].config.data.title}
-        column={chartData[0].columns}
-        masterDataset={master[6]}
-        dataset={chartData[0].config.data}
-        options={options.pieOptions}
-      /></div>
-      <div > <PieChart
-        title={chartData[0].config.data.title}
-        column={chartData[0].columns}
-        masterDataset={master[6]}
-        dataset={chartData[0].config.data}
-        options={options.pieOptions}
-      /></div>
-      <div >Box5-Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam nam dicta molestiae quo, placeat sapiente
-        soluta ducimus aspernatur qui magni distinctio sint nostrum repellendus id rerum, eligendi similique eum
-        enim tempora. Asperiores, perspiciatis ex sit inventore maiores deleniti sed in corrupti numquam pariatur
-        ipsum mollitia impedit molestias modi nesciunt illo sint vitae tempora sequi id totam voluptate soluta nihil
-        explicabo. Quae, eos eaque dolorum ad assumenda, esse eum deserunt nihil tempore debitis autem doloribus
-        quas natus pariatur maxime cum unde!
-        Box2-Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet et vitae omnis, iusto cupiditate commodi
-        debitis sunt praesentium quis. Repellendus tenetur expedita eius. Et quos provident architecto numquam vitae
-        cumque voluptatem quaerat, delectus recusandae officia nesciunt quod tempore harum dolores nobis neque
-        veniam est eveniet dolore assumenda eos culpa! Pariatur, iste nobis, corrupti doloribus optio numquam, eum
-        nihil reiciendis deleniti maxime explicabo a harum. Architecto unde vitae pariatur tempora ab veniam
-        asperiores, facere quod beatae, sint vero aut dolorem quis.</div>
+        <PieChart
+          title={chartData[0].config.data.title}
+          column={chartData[0].columns}
+          masterDataset={master[6]}
+          dataset={chartData[0].config.data}
+          options={options.pieOptions}
+        />
+      </div>
+      <div style={{textAlign: 'center'}}>
+        <h3>Pandemic Impact on Mental Health</h3>
+        <p>
+          On May 11, the South Texas Trauma-Informed Care Consortium Honors
+          Trauma-Informed Care Month with Events and Training to Support
+          Healing From Trauma hosted a a press conference with the City of
+          San Antonio, University Health, The Ecumenical Center, The Children's
+          Shelter, Methodist Healthcare Ministries, and Voices for Children, to
+          launch a summer awareness campaign to support resilience and trauma-informed
+          practices. They specifically referenced a recent Bexar Facts poll, highlighting
+          that half of Bexar County residents reported that the pandemic had adversely
+          affected their mental health.
+        </p>
+        <p>
+          We were able to amplify  the collective weight of the shared trauma
+          of the pandemic that is beginning to be measured, more than a year
+          after COVID-19 arrived in the San Antonio community.
+        </p>
+      </div>
+      <div>
+        <PieChart
+          title={chartData[1].config.data.title}
+          column={chartData[1].columns}
+          masterDataset={master[6]}
+          dataset={chartData[1].config.data}
+          options={options.pieOptions}
+        />
+      </div>
     </div>
   )
 }
